89a79f7b14453effbeb8b1c8baa66d3bd6187316
Ubaldu <oblako48@gmail.com>
2023-04-21 20:38:24 +0300

    Update

--- Selenium.py
+++ Selenium.py
@@ -452,16 +452,16 @@
 
 #=======================================================================================================================
 
-# # from time import sleep
-# # from selenium import webdriver
-# # from selenium.webdriver.common.by import By
-# # from selenium.webdriver.common.keys import Keys
-# # from selenium.webdriver.support.select import Select
-# # from selenium.webdriver.support.wait import WebDriverWait
-# # from selenium.webdriver.support import expected_conditions
-# # from selenium.webdriver.common.action_chains import ActionChains
-# # from selenium.webdriver.chrome.options import Options
-# # import unittest
+# from time import sleep
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.support.select import Select
+# from selenium.webdriver.support.wait import WebDriverWait
+# from selenium.webdriver.support import expected_conditions
+# from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.chrome.options import Options
+# import unittest
 
 # class SiteTesting(unittest.TestCase):
     
@@ -485,18 +485,18 @@
     
 #=====================================================================================================
 
-import requests
-from requests.exceptions import HTTPError
+# import requests
+# from requests.exceptions import HTTPError
  
-for url in ['https://api.github.com', 'https://api.github.com/invalid']:
-    try:
-        response = requests.get(url)
+# for url in ['https://api.github.com', 'https://api.github.com/invalid']:
+#     try:
+#         response = requests.get(url)
  
-        # если ответ успешен, исключения задействованы не будут
-        response.raise_for_status()
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')  # Python 3.6
-    except Exception as err:
-        print(f'Other error occurred: {err}')  # Python 3.6
-    else:
-        print('Success!')
+#         # если ответ успешен, исключения задействованы не будут
+#         response.raise_for_status()
+#     except HTTPError as http_err:
+#         print(f'HTTP error occurred: {http_err}')  # Python 3.6
+#     except Exception as err:
+#         print(f'Other error occurred: {err}')  # Python 3.6
+#     else:
+#         print('Success!')
